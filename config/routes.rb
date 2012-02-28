22d4d61df03e67842fb3bd4e3f57a92378b89c9c
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-28 14:30:34 +0200

    ch05: 5.3 bitti

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 T::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
