8de22f772ad35e884119659945a5e3abf1cccd33
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-28 22:58:08 +0200

    ch06: listing 6.5 e ulaştık

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+require 'spec_helper'
+
+describe User do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
