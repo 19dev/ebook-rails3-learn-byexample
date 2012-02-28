a0fe6c5c23c0aad07e3913ade2826e5d340c27fc
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-29 00:36:29 +0200

    ch06: 6.3.2 bitti

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,8 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: 'Example User', email: 'e@mail.com')
+    @user = User.new(name: 'Example User', email: 'e@mail.com',
+                    password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -21,6 +22,8 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -31,6 +34,16 @@ describe User do
 
   describe "when email is not present" do
     before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.pasword_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.pasword_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 
