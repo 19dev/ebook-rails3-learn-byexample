bf7dba80f1be9dc8cb7f87e64e4824787755a9ec
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-28 19:37:02 +0200

    ch05: 5.4.1 Users new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 T::Application.routes.draw do
+  get "users/new"
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
