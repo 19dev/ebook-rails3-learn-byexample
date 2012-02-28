414f1330116ce3bd34c4cd4d06c18684e802a7a5
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-28 19:46:38 +0200

    ch05: /signup yönlendirmesi tamam

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 T::Application.routes.draw do
   get "users/new"
 
+  match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
