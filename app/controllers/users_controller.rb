6976ef683aa2a134e9187aef1f31797cecebbee1
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-29 09:47:05 +0200

    ch07: 7.3.1 bitti

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+        @user = User.new(params[:user])
+        if @user.save
+          # Handle a successful save.
+        else
+          render 'new'
+        end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,28 @@ describe "User pages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button "Sign up" }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
