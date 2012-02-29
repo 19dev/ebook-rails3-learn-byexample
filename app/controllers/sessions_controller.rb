06f8ad91ed5af691525cb455735c79ea7429c64f
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-29 13:57:42 +0200

    ch08: migration öncesi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+	  	else
+			flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+			render 'new'
+  		end
 	end
 
 	def destroy
