9d77f570aba05f6463eff538e36f9594e1cdbc4b
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-29 03:13:07 +0200

    ch06_v2: 6.2.4 e ulaştık

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
-require 'spec_helper'
-
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
 end
