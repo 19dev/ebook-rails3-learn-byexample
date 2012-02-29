1fa1dabbe55394d756bd975b1e9a456b4be7dcff
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-29 10:39:12 +0200

    ch08: 8.1.1 bitti

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 T::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup',  to: 'users#new'
+  match '/signup',   to: 'users#new'
+  match '/signin',   to: 'sessions#new'
+  match '/signout',  to: 'sessions#destroy', via: :delete
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1', text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end
+end
