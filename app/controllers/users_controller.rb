144e8226a1e697384ab1ba1341c74d53cb5511b7
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-02-29 18:58:35 +0200

    ch09: devam

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,17 @@ class UsersController < ApplicationController
           render 'new'
         end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 end
