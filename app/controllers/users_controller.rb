3c231790d8e0ce10356fcaff5855031ba054d668
nurettin senyer <seyyah@bil.omu.edu.tr>
2012-03-08 18:23:55 +0200

    ch07: ssl aşaması da eklendi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
         @user = User.new(params[:user])
         if @user.save
-          # Handle a successful save.
+          flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user
         else
           render 'new'
         end
